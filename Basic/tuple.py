de8aaa76977223790a35a521f1b712ffb5f4dbde
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-01-10 15:11:35 +0530

    new data type tuple

---
+++ Basic/tuple.py
@@ -0,0 +1,26 @@
+# tuple
+
+list = (2,3,4,5,6,7,5,5)
+
+# list[0]=5 -> error: 'tuple' object does not support item assignment
+
+print(list.count(5))  # count the element
+
+# find the index
+print(list.index(2)) 
+print(list.index(4))
+
+print(list)
+#tuple
+name = "raj", "rohan", "manmeet", "Megha", "amresh"
+print(name)
+
+
+# Output
+"""
+3
+0
+2
+(2, 3, 4, 5, 6, 7, 5, 5)
+('raj', 'rohan', 'manmeet', 'Megha', 'amresh')
+"""
