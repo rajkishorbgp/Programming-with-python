a820b1267a2960abeaf69a4598649ee66269a1ef
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-01-10 15:09:28 +0530

    new data type dictionary

---
+++ Basic/dictionary.py
@@ -0,0 +1,14 @@
+# dictionary
+marks= {"English": 95, "java":98, "C": 96, "C++": 95, "python": 97}
+
+print(marks["C"])   # print key value
+marks["English"]=90  # change value
+marks["markup"]=90  # insert
+
+print(marks)
+
+#  Output
+"""
+96
+{'English': 90, 'java': 98, 'C': 96, 'C++': 95, 'python': 97, 'markup': 90}
+"""
