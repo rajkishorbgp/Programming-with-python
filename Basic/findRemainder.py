d51f0be120221f33cf1bd46997d289f8473f9e4a
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-05-10 20:49:11 +0530

    find Remainder

---
+++ Basic/findRemainder.py
@@ -0,0 +1,15 @@
+a = input("Enter the value of a: ")
+z = input("Enter the value of z: ")
+
+rem = int(a) % int(z)
+
+print("rem: ", rem)
+
+
+'''
+Output:
+Enter the value of a: 30
+Enter the value of z: 7
+rem:  2
+
+'''
