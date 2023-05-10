2f4a9519611207081a6e90d09300ad04818d872f
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-05-10 21:04:02 +0530

    data types

---
+++ dataTypes.py
@@ -0,0 +1,47 @@
+
+
+# integer
+a = 100
+
+# float
+b = 2.3
+
+# string
+name = 'Raj'
+name1 = "Rajkishor"
+name2 = ''' Raj ' " '"  '''
+
+# booleans
+c = True
+
+# none
+d = None
+
+print(a)
+print(b)
+print(name)
+print(name1)
+print(name2)
+print(c)
+print(d)
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(name))
+
+'''
+100
+2.3
+Raj
+Rajkishor
+ Raj ' " '"  
+True
+None
+<class 'int'>
+<class 'float'>
+<class 'bool'>
+<class 'str'>
+
+'''
