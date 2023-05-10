34e2f8b3adc087730bb9eb4d14d744b8befed3b1
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-05-10 20:42:05 +0530

    average two number

---
+++ Basic/averageTwoNumber.py
@@ -0,0 +1,13 @@
+num1 = input("Enter the first Number: ")
+num2 = input("Enter the second Number: ")
+
+average = (int(num1)+int(num2))/2
+
+print("Average of two number: ", average)
+
+'''
+Output:
+Enter the first Number: 70
+Enter the second Number: 70
+Average of two number:  70.0
+'''
