ea118b1e5ea9b51339bfdb49c145dff953055c11
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-05-10 20:48:21 +0530

    comperision operators

---
+++ Basic/comperisionOperators.py
@@ -0,0 +1,14 @@
+a = input("Enter the value of a: ")
+b = input("Enter the value of b: ")
+
+print("\na = ", a, "b = ", b)
+print("a>b -> ", a > b)
+
+'''
+Output:
+Enter the value of a: 20
+Enter the value of b: 30
+
+a =  20 b =  30
+a>b ->  False
+'''
