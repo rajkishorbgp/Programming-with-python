9b7a5848d97663ef0f9dc9aecfe3d53c554f4f1e
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-05-10 20:41:03 +0530

    add two number

---
+++ Basic/addTwoNumber.py
@@ -0,0 +1,12 @@
+a = input("Enter the value a: ")
+b = input("Enter the value b: ")
+
+sum = int(a)+int(b)
+print("sum: ", sum)
+
+'''
+Output:
+Enter the value a: 26
+Enter the value b: 26
+sum:  52
+'''
