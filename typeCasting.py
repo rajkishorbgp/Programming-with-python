95cd0ccb7410b7f24dbfd2f172ab4d5eb27c7217
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-05-10 21:11:35 +0530

    type casting

---
+++ typeCasting.py
@@ -0,0 +1,21 @@
+str = '200'
+
+print(str)
+
+#  typeCasting
+b = int(str)+10
+print(b)
+
+'''
+
+name = 'a' #ValueError: invalid literal for int() with base 10: 'a'
+print(type(name))
+print(int(name))
+
+'''
+
+'''
+Output
+200
+210
+'''
