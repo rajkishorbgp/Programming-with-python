5a165c1d80e266c3ce8756084fa6c657de4c2d1b
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-05-10 21:05:25 +0530

    input function

---
+++ inputFunction.py
@@ -0,0 +1,20 @@
+a = input("Enter the number: ")
+
+print(type(a))
+print(a)
+
+
+a = int(a)+10
+print(type(a))  # Convert a to an Integer( if possible)
+print(a)
+
+'''
+
+Output
+Enter the number: 100
+<class 'str'>
+100
+<class 'int'>
+110
+
+'''
